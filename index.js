963feaa6221970cb690eb5cd0c813d2186aedc74
Codelleschi <a.steinhauslin@gmail.com>
2022-05-11 21:50:43 -0700

    port typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require("./server")
 
-const PORT = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
-server.listen(PORT, () => {
-    console.log(`server started on port ${ PORT }`)
+server.listen(port, () => {
+    console.log(`server started on port ${ port }`)
 })
