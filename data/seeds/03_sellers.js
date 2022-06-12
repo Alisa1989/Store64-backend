464767a236efe5683c956d27244bacc8b4bc8e45
Codelleschi <a.steinhauslin@gmail.com>
2022-06-11 21:54:23 -0700

    added data to sellers seeding file

--- data/seeds/03_sellers.js
+++ data/seeds/03_sellers.js
@@ -2,9 +2,13 @@ exports.seed = async function (knex) {
     await knex("sellers").insert([
         {
             companyName: "sell4less",
+            email: "info@sell4less.com",
+            rating: 4.7,
+            numberReviews: 1426
         },
         {
             companyName: "JoyOfSelling",
+            email: "JoyOfSelling@outlook.com"
         },
     ]);
 };
